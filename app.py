72b7c27a30f10ea0b654d1988cfcf0b21df91fae
sabbitidileepkumar-netizen <sabbitidileepkumar@gmail.com>
2026-08-23 13:11:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def chat():
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.1-flash-lite",
             contents=context
         )
         reply = response.text
